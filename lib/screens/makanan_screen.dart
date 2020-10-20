a4880669edb0b62d63f210a3d9315e75cd87c8f3
Iswandi Saputra <iswandi.saputra11@gmail.com>
2020-10-20 09:31:05 +0700

    web makanan

--- lib/screens/makanan_screen.dart
+++ lib/screens/makanan_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/model/model_makanan.dart';
 import 'package:flutter_app/ui/ui_makanan.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 
 class MakananScreen extends StatelessWidget {
-  UiMakanan uiMakanan = UiMakanan();
+ final UiMakanan uiMakanan = UiMakanan();
 
   @override
   Widget build(BuildContext context) {
@@ -16,31 +17,43 @@ class MakananScreen extends StatelessWidget {
 }
 
 class DetailMakanan extends StatelessWidget {
-  UiMakanan uiMakanan = UiMakanan();
-  ModelMakanan makanan;
-  
+ final UiMakanan uiMakanan = UiMakanan();
+ final ModelMakanan makanan;
+
   // DetailMakanan(this.makanan);
-  DetailMakanan({Key key,@required this.makanan}) : super(key: key);
+  DetailMakanan({Key key, @required this.makanan}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Detail Makanan"),
-      actions: [
-        IconButton(icon: Icon(Icons.open_in_browser), onPressed: (){
-
-        })
-      ],),
+      appBar: AppBar(
+        title: Text("Detail Makanan"),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.open_in_browser),
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => WebMakanan(makanan: makanan)));
+              })
+        ],
+      ),
       body: uiMakanan.buildDetailList(makanan),
     );
   }
 }
-
 class WebMakanan extends StatelessWidget {
+ final ModelMakanan makanan;
+
+  WebMakanan({Key key, @required this.makanan}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return WebviewScaffold(
+      appBar: AppBar(
+        title: Text(makanan.namaMkn),
+      ),
+      url: makanan.urlMkn,
     );
   }
 }
